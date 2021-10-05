d3ad5516ac919f164b13b9c42b76c7fe9bbae66b
guntiger7 <guntiger.choi@gmail.com>
2021-10-05 14:34:34 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstLocal = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lstLocal
+            // 
+            this.lstLocal.HideSelection = false;
+            this.lstLocal.Location = new System.Drawing.Point(157, 72);
+            this.lstLocal.Name = "lstLocal";
+            this.lstLocal.Size = new System.Drawing.Size(469, 279);
+            this.lstLocal.TabIndex = 0;
+            this.lstLocal.UseCompatibleStateImageBehavior = false;
+            this.lstLocal.SelectedIndexChanged += new System.EventHandler(this.lstLocal_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstLocal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstLocal;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstLocal.View = View.Details;
+            lstLocal.Clear();
+            lstLocal.GridLines = true;
+            lstLocal.FullRowSelect = true;
+            lstLocal.BackColor = System.Drawing.Color.Aquamarine;
+            lstLocal.Columns.Add("IP", 100);
+            lstLocal.Columns.Add("HostName", 200);
+            lstLocal.Columns.Add("MAC Address", 300);
+
+            Ping_all();
+        }
+
+        //private string NetworkGateway()
+        //{
+        //    string ip = null;
+        //    foreach (NetworkInterface f in NetworkInterface.GetAllNetworkInterfaces())
+        //    {
+        //        if (f.OperationalStatus == OperationalStatus.Up)
+        //        {
+        //            foreach (GatewayIPAddressInformation d in f.GetIPProperties().GatewayAddresses)
+        //            {
+        //                ip = d.Address.ToString();
+        //            }
+        //        }
+        //    }
+        //    return ip;
+        //}
+
+        public void Ping_all()
+        {
+            //string gate_ip = NetworkGateway();
+            //string[] array = gate_ip.Split('.');
+            
+            for(int i= 2; i<=255; i++)
+            {
+                string ping_var = "192.168.137." + i;
+                Ping(ping_var, 4, 4000);
+            }
+        }
+
+        public void Ping(string host, int attempts, int timeout)
+        {
+            for(int i = 0; i < attempts; i++)
+            {
+                new Thread(delegate ()
+                {
+                    try
+                    {
+                        System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+                        ping.PingCompleted += new PingCompletedEventHandler(PingCompleated);
+                        ping.SendAsync(host, timeout, host);
+                    }
+                    catch
+                    {
+
+                    }
+                }).Start();
+            }
+        }
+
+        private void PingCompleated(object sender, PingCompletedEventArgs e)
+        {
+            string ip = (string)e.UserState;
+            if(e.Reply != null && e.Reply.Status == IPStatus.Success)
+            {
+                string hostname = GetHostName(ip);
+                string macaddress = GetMacAddress(ip);
+                string[] arr = new string[3];
+
+                arr[0] = ip;
+                arr[1] = hostname;
+                arr[2] = macaddress;
+
+                ListViewItem item;
+                if(this.InvokeRequired)
+                {
+                    this.Invoke(new Action(() =>
+                        {
+                            item = new ListViewItem(arr);
+                            lstLocal.Items.Add(item);
+                        }));
+                }
+                else
+                {
+                    MessageBox.Show(e.Reply.Status.ToString());
+                }
+            }
+        }
+
+        public string GetHostName(string ipAddress)
+        {
+            try
+            {
+                IPHostEntry entry = Dns.GetHostEntry(ipAddress);
+                if(entry != null)
+                {
+                    
+                    return entry.HostName;
+                }
+            }catch(SocketException)
+            {
+
+            }
+            return null;
+        }
+        public string GetMacAddress(string ipAddress)
+        {
+            string macAddress = string.Empty;
+            System.Diagnostics.Process Process = new System.Diagnostics.Process();
+            Process.StartInfo.FileName = "arp";
+            Process.StartInfo.Arguments = "-a " + ipAddress;
+            Process.StartInfo.UseShellExecute = false;
+            Process.StartInfo.RedirectStandardOutput = true;
+            Process.StartInfo.CreateNoWindow = true;
+            Process.Start();
+            string strOutput = Process.StandardOutput.ReadToEnd();
+            string[] substrings = strOutput.Split('-');
+            if(substrings.Length >= 8)
+            {
+                macAddress = substrings[3].Substring(Math.Max(0, substrings[3].Length - 2)) + "-" + substrings[4] + "-" + substrings[5] +
+                    "-" + substrings[6] + "-" + substrings[7] + "-" + substrings[8].Substring(0, 2);
+                return macAddress;
+            }
+            else
+            {
+                return "OwnedForms Machin";
+            }
+        }
+
+        private void lstLocal_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
